6396e1655d5399c4cd9021b30beeb4247b57afe5
Miriam Reyes <neurona13@gmail.com>
2018-07-21 17:23:55 -0500

    Separete file examples

---
+++ regularVerbs/examplesVerbs.js
@@ -0,0 +1,38 @@
+var examples= {
+laugh	  :"",	
+cough	  :"",
+advise   :"",
+argue    :"She argued that it would be cheaper to travel by bus./\
+       	I’m not going to argue/Will you children stop arguing with each other about whose toy that is!/\
+       	Persuadir (positivo): I’ll try to argue him into going;disuadir de (negativo): He argued her out of buying the dress/\
+       	Sostener con argumentos",
+apply	  :"",
+appear   :"",
+answer   :"",
+allow    :"",
+aim      :"Aspirar, pretender:We’re aiming for a ten percent increase in sales/\
+       Apuntar:He aimed the gun at the target/\
+       Pretender, tener la intención, proponerse:He aims at finishing tomorrow",
+agree    :"",
+affect   :"",
+admit    :"",
+add      :"",
+act      :"",
+achieve  :"We finally achieved our goal of buying a house",
+accept   :"",
+clean    :"",
+claim    :"Reclamar:She claimed $2,500 in travel expenses./\
+       Afirmar, pretender:She claimed that the dog attacked her./\
+       There were claims that.../\
+       Did anyone claim the umbrella which was left behind?/\
+       He claims to be the fastest runner in the class./\
+       You can claim your money back if the goods are damaged.",
+charge   :"Cobrar:How much do you charge for delivery?/\
+       Acusar:He has been charged with murder/\
+       Cargar un celular/\
+       Embestir, arremeter:The bull charged at the boy/\
+       Correr: The children charged out of the school.",
+cause   : "",
+carry   : "He was carrying my bags/She still carries his photo in her wallet/\
+	Trasportar en aviones o mosquitos enfermedades"
+}
